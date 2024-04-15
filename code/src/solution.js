b13ad0f9e42b0015d248801eff7b9540d7eeafa5
Ajay Kumar Medikonda <ajaykumar.medikonda@slu.edu>
2024-04-15 12:29:44 -0500

    changed the ground sizes

--- code/src/solution.js
+++ code/src/solution.js
@@ -12,7 +12,7 @@ let ballVelocity = new THREE.Vector3();
 let movementSpeed = 0.1;
 let rotationSpeed = 0.5;
 let ballRadius = 20;
-let groundSize = 1000;
+let groundSize = 5000;
 let hdrs = [
     '../assets/hdr/golden.hdr', // Center ground
     '../assets/hdr/cloudy.hdr', // Right ground
@@ -26,15 +26,15 @@ let models={
 
 window.init = async (canvas) => {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 10000);
     renderer = new THREE.WebGLRenderer({ canvas });
     scene.fog = new THREE.Fog(0xffffff, 0, 1000); //fog
 
     createGround(0x336159, 0, 0, Math.PI / 2, 'assets/img/sky1.jpeg',30,'../assets/img/rock1.jpg'); // Center ground
-    createGround(0xff0000, 1000, 0, Math.PI / 2,'assets/img/river1.jpg',20,'../assets/img/arid2_bk.jpg'); // Right ground
+    createGround(0xff0000, 5000, 0, Math.PI / 2,'assets/img/river1.jpg',20,'../assets/img/arid2_bk.jpg'); // Right ground
     //createGround(0x0000ff, -1000, 0, Math.PI / 2,'assets/img/thunder1.jpg'); // Left ground
-    createGround(0xffff00, 0, -1000, Math.PI / 2,'assets/img/sky1.jpeg',15,'../assets/img/river1.jpg'); // Back ground
-    createGround(0x00ffff, 1000, -1000, Math.PI / 2,'assets/img/sky1.jpeg',10,'../assets/img/sky1.jpeg'); // Back right ground
+    createGround(0xffff00, 0, -5000, Math.PI / 2,'assets/img/sky1.jpeg',15,'../assets/img/river1.jpg'); // Back ground
+    createGround(0x00ffff, 5000, -5000, Math.PI / 2,'assets/img/sky1.jpeg',10,'../assets/img/sky1.jpeg'); // Back right ground
 
     ball = createBall();
     scene.add(ball);
@@ -79,7 +79,7 @@ function createGround(color, x, z, rotation, texturePath, numCubes,image) {
     const texture = textureLoader.load(texturePath);
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(1, 1); //repetition as needed
+    texture.repeat.set(2, 2); //repetition as needed
     const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
     const ground = new THREE.Mesh(geometry, material);
     ground.rotation.x = rotation;
@@ -116,10 +116,10 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-let minX = -500 + ballRadius;
-let maxX = 500 - ballRadius;
-let minZ = -500 + ballRadius;
-let maxZ = 450 - ballRadius;
+let minX = -2500 + ballRadius;
+let maxX = 2500 - ballRadius;
+let minZ = -2500 + ballRadius;
+let maxZ = 2450 - ballRadius;
 
 
 window.loop = () => {
@@ -127,16 +127,16 @@ window.loop = () => {
     checkCollision();
     ball.position.add(ballVelocity);
     
-    if(camera.position.x>500 && camera.position.z>-400){
+    if(camera.position.x>2500 && camera.position.z>-2400){
         setback(2);
     }
-    else if(camera.position.x>500 &&camera.position.z<-400){
+    else if(camera.position.x>2500 &&camera.position.z<-2400){
         setback(4);
     }
-    else if(camera.position.x<500 && camera.position.z<-400){
+    else if(camera.position.x<2500 && camera.position.z<-2400){
         setback(3);
     }
-    else if(camera.position.x<500 && camera.position.z>-400){
+    else if(camera.position.x<2500 && camera.position.z>-2400){
         setback(1);
     }
     if(cubeCount==10||cubeCount==25 ||cubeCount==5 ||cubeCount==20){
@@ -149,7 +149,7 @@ window.loop = () => {
     ball.position.z = THREE.MathUtils.clamp(ball.position.z, minZ, maxZ);
 
     camera.position.x = ball.position.x;
-    camera.position.z = ball.position.z+90;
+    camera.position.z = ball.position.z+100;
     camera.lookAt(ball.position);
     renderer.render(scene, camera);
 };
@@ -193,7 +193,7 @@ const cubes = [];
 
 function addRandomCubes(positionX, positionZ, numCubes,image) {
     const cubeSize = 15; // Size of each cube
-    const maxPosition = 500; // Maximum position range
+    const maxPosition = 2500; // Maximum position range
     const textureLoader = new THREE.TextureLoader();
     const texture = textureLoader.load(image); //texture image
 
@@ -256,9 +256,9 @@ function setback(n){
         }
         n2=1;n3=1;n4=1;
         if(cubeCount>5 && cubeCount<10){
-            maxX=1500-ballRadius;
-            if(ball.position.x>500){
-                minX=500+ballRadius;
+            maxX=7500-ballRadius;
+            if(ball.position.x>7500){
+                minX=7500+ballRadius;
             }
         }
     }
@@ -276,7 +276,7 @@ function setback(n){
             n1=1;n3=1;n4=1;
             if(cubeCount>10){
                 //minX=500-ballRadius;
-                minZ=-1500+ballRadius; 
+                minZ=-7500+ballRadius; 
                                
             }
     }
@@ -284,7 +284,7 @@ function setback(n){
         if(n3==1){
             //scene.background=new THREE.Color(0xff0000);
             console.log("ground3");
-            scene.fog = new THREE.Fog(0xffffff, 0, 500);
+            scene.fog = new THREE.Fog(0xffffff, 0, 7500);
             new RGBELoader().load(hdrs[2],(environmentMap)=>{
     
                 //environmentMap.map=THREE.EquirectangularRefractionMapping;
@@ -299,7 +299,7 @@ function setback(n){
         if(n4==1){
             //scene.background=new THREE.Color(0xff0000);
             console.log("ground4");
-            scene.fog = new THREE.Fog(0xffffff, 0, 500);
+            scene.fog = new THREE.Fog(0xffffff, 0, 7500);
             new RGBELoader().load(hdrs[2],(environmentMap)=>{
     
                 //environmentMap.map=THREE.EquirectangularRefractionMapping;
@@ -308,9 +308,9 @@ function setback(n){
             n4=n4+1;
             }
             n1=1;n2=1;n3=1;
-            if(ball.position.z<-500){
-                maxZ=-500-ballRadius;
-                minX=-500+ballRadius;
+            if(ball.position.z<-7500){
+                maxZ=-7500-ballRadius;
+                minX=-7500+ballRadius;
             }
     }
 }
